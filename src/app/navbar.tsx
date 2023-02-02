bfbe0e53f796ebc7fc86cad1c8e77558074595d5
mssalkhalifah <mssalkhalifah@gmail.com>
2023-02-02 19:22:37 +0300

    feat: Updated the navbar with newer visuals and functionalities

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,27 +1,92 @@
 "use client";
 
+import Link from "next/link";
+import { ReactNode, useState } from "react";
+import {
+  FaFolder,
+  FaFolderOpen,
+  FaHome,
+  FaBlogger,
+  FaSun,
+  FaMoon,
+} from "react-icons/fa";
+import { Url } from "url";
+
 interface NavButton {
   title?: string;
   className?: string;
+  iconTypeOpen?: ReactNode;
+  iconTypeClose?: ReactNode;
+  url?: string;
 }
 
-function NavButton({ title, className }: NavButton) {
+function NavButton({
+  title,
+  className,
+  iconTypeOpen,
+  iconTypeClose,
+  url,
+}: NavButton) {
+  const [mouseOver, setMouseOver] = useState(false);
+
   return (
-    <button className={`p-2 bg-white rounded-md w-24 ${className}`}>
-      {title}
+    <button
+      onMouseOver={() => setMouseOver(true)}
+      onMouseLeave={() => setMouseOver(false)}
+      className={`p-2 bg-white rounded-lg w-24 shadow-md ${className}`}
+    >
+      <Link href={url ? url : ""}>
+        <div className="flex flex-row place-items-center justify-center">
+          <span className="mr-1">
+            {mouseOver ? iconTypeOpen : iconTypeClose}
+          </span>
+          {title}
+        </div>
+      </Link>
+    </button>
+  );
+}
+
+function DarkModeToggle() {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <button
+      className="p-2 bg-white rounded-md shadow-md"
+      onClick={() => setToggle(!toggle)}
+    >
+      {toggle ? <FaSun /> : <FaMoon />}
     </button>
   );
 }
 
 export default function Navbar() {
   return (
-    <div className="flex w-full bg-blue-300 p-2 justify-between">
+    <div className="flex w-full mt-4 justify-between place-items-center pointer-events-auto">
       <div>
-        <NavButton title="Home" />
-        <NavButton title="Projects" className="mx-4" />
-        <NavButton title="Blogs" />
+        <NavButton
+          title="Home"
+          iconTypeOpen={<FaHome />}
+          iconTypeClose={<FaHome />}
+          url="/"
+        />
+        <NavButton
+          title="Projects"
+          className="mx-4"
+          iconTypeOpen={<FaFolderOpen />}
+          iconTypeClose={<FaFolder />}
+          url="/projects"
+        />
+        <NavButton
+          title="Blogs"
+          iconTypeOpen={<FaBlogger />}
+          iconTypeClose={<FaBlogger />}
+          url="/blogs"
+        />
       </div>
-      <div></div>
+      <div>
+        <DarkModeToggle />
+      </div>
     </div>
   );
 }
