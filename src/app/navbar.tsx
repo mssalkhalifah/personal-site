fa7468d548eedb59676a62798eafc0305edc3fc8
mssalkhalifah <mssalkhalifah@gmail.com>
2023-02-02 17:23:53 +0300

    feat: basic nav buttons

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,14 +1,27 @@
 "use client";
 
+interface NavButton {
+  title?: string;
+  className?: string;
+}
+
+function NavButton({ title, className }: NavButton) {
+  return (
+    <button className={`p-2 bg-white rounded-md w-24 ${className}`}>
+      {title}
+    </button>
+  );
+}
+
 export default function Navbar() {
   return (
-    <div className="flex w-full bg-blue-300 p-2">
-      <button
-        className="p-2 bg-white rounded-md"
-        onClick={() => console.log("Hello")}
-      >
-        Hello
-      </button>
+    <div className="flex w-full bg-blue-300 p-2 justify-between">
+      <div>
+        <NavButton title="Home" />
+        <NavButton title="Projects" className="mx-4" />
+        <NavButton title="Blogs" />
+      </div>
+      <div></div>
     </div>
   );
 }
