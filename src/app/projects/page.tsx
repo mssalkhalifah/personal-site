cb90f0b5c0538e7b90f2972082b7f526a8a9522c
Mohammad Alkhalifah <60398196+mssalkhalifah@users.noreply.github.com>
2023-02-12 21:55:35 +0300

    feat: Added basic props properties to Card component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,8 @@ import Card from "@/components/Card";
 export default function page() {
   return (
     <div>
-      <Card />
+      <Card title="Personal Site" date="12/09/2023 - current" />
+      <Card title="Personal Site" date="12/09/2023 - current" />
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import placeholder from "../../public/thumb_placeholder.png";
 
-export default function Card() {
+interface CardProps {
+  title?: string;
+  date?: string;
+}
+
+export default function Card({ title, date }: CardProps) {
   return (
     <div className="flex flex-col">
       <Image src={placeholder} width={320} height={180} alt={""} />
       <div className="mt-1 flex flex-col text-center">
-        <h3 className="text-2xl font-bold">Title</h3>
-        <p className="text-lg">00/00/20XX - 00/00/20XX</p>
+        <h3 className="text-2xl font-bold">{title || "Title"}</h3>
+        <p className="text-lg">{date || "00/00/20XX - 00/00/20XX"}</p>
       </div>
     </div>
   );
