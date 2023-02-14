cb9b27e7e35436dad5a76854a6586b88c12895f0
Mohammad Alkhalifah <60398196+mssalkhalifah@users.noreply.github.com>
2023-02-14 18:49:15 +0300

    feat: Added loading page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,13 @@
 import Gallary from "@/components/gallary";
-
+/*
+* {
+    "albumId": 1,
+    "id": 1,
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+  }
+ */
 interface Project {
   title?: string;
   imgUrl?: string;
@@ -7,12 +15,16 @@ interface Project {
 }
 
 async function getProjects() {
-  const projects: Project[] = [
-    { title: "1Hello World" },
-    { title: "2World" },
-    { title: "3World Hello" },
-    { title: "4World Hello" },
-  ];
+  const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+  const resJson = await res.json();
+  const projects: Project[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    projects.push({
+      title: resJson[i].id,
+      imgUrl: resJson[i].thumbnailUrl,
+    });
+  }
 
   return projects;
 }
