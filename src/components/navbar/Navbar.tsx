b125fd874dafc111c9e2556404a8a99c87d7d830
Mohammad Alkhalifah <60398196+mssalkhalifah@users.noreply.github.com>
2023-03-02 14:42:55 +0300

    feat: Made navbar updates with scroll event

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import useScrollPosition from "@/hooks/useScrollPosition";
 import Link from "next/link";
-import { ReactNode, useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { ReactNode, useCallback, useEffect, useState } from "react";
 import { FaFolder, FaFolderOpen, FaHome, FaBlogger } from "react-icons/fa";
 
 export interface INavbar {}
@@ -13,6 +15,7 @@ interface NavButton {
   iconTypeClose?: ReactNode;
   url?: string;
   onClick?: () => void;
+  focus?: boolean;
 }
 
 function NavButton({
@@ -22,15 +25,17 @@ function NavButton({
   iconTypeClose,
   url,
   onClick,
+  focus = false,
 }: NavButton) {
   const [mouseOver, setMouseOver] = useState(false);
+  const focusedStyle = focus ? "bg-gray-500 shadow-lg" : "";
 
   return (
     <button
       onMouseOver={() => setMouseOver(true)}
       onMouseLeave={() => setMouseOver(false)}
       onClick={onClick && (() => onClick())}
-      className={`w-28 rounded-md bg-gray-700 p-2 text-white shadow-lg transition hover:scale-110 ${className}`}
+      className={`w-28 rounded-md ${focusedStyle} p-2 text-white transition duration-300 hover:scale-110 ${className}`}
     >
       <Link href={url ? url : ""}>
         <div className="flex flex-row place-items-center justify-center">
@@ -45,31 +50,64 @@ function NavButton({
 }
 
 const Navbar: React.FC<INavbar> = ({}) => {
-  const [top, setTop] = useState("");
+  const scrollPosition = useScrollPosition();
+  const pathname = usePathname();
+  const [focus, setFocus] = useState([true, false, false]);
+  const [scrollTo, setScrollTo] = useState(0);
+  const [sectionElements, setSectionElements] = useState<
+    (HTMLElement | null)[]
+  >([]);
   const topStyle = "bg-zinc-500/50 backdrop-blur-md";
 
-  const onScroll = () => {
-    if (window.pageYOffset === 0) {
-      console.log("Scroll is at top");
-      setTop("");
-    } else {
-      setTop(topStyle);
+  useEffect(() => {
+    if (pathname?.length === 1) {
+      setSectionElements([
+        document.getElementById("TopSection"),
+        document.getElementById("ProjectSection"),
+        document.getElementById("AboutSection"),
+      ]);
     }
-  };
+  }, [pathname]);
 
   useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-
-    if (window.pageYOffset !== 0) {
-      setTop(topStyle);
+    const newFocusList = [false, false, false];
+    if (
+      sectionElements[0] &&
+      sectionElements[1] &&
+      sectionElements[2] &&
+      pathname?.length === 1
+    ) {
+      if (
+        scrollPosition >= sectionElements[0]?.offsetTop &&
+        scrollPosition < sectionElements[1]?.offsetTop
+      ) {
+        newFocusList.splice(0, 1, true);
+      } else if (
+        scrollPosition >= sectionElements[1]?.offsetTop &&
+        scrollPosition < sectionElements[2]?.offsetTop
+      ) {
+        newFocusList.splice(1, 1, true);
+      } else if (scrollPosition >= sectionElements[2]?.offsetTop) {
+        newFocusList.splice(2, 1, true);
+      }
+      setFocus(newFocusList);
+    } else {
+      setFocus([false, false, false]);
     }
+  }, [pathname, scrollPosition, scrollTo, sectionElements]);
 
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
+  useEffect(() => {
+    window.scrollTo({
+      top: sectionElements[scrollTo]?.offsetTop,
+      behavior: "smooth",
+    });
+  }, [scrollTo, sectionElements]);
 
   return (
     <nav
-      className={`fixed top-0 z-[5] flex w-full place-items-center justify-around p-2 transition-colors ${top}`}
+      className={`fixed top-0 z-[5] flex w-full place-items-center justify-between p-2 transition-colors ${
+        scrollPosition > 0 && topStyle
+      }`}
     >
       <div>
         <NavButton
@@ -77,41 +115,28 @@ const Navbar: React.FC<INavbar> = ({}) => {
           iconTypeOpen={<FaHome />}
           iconTypeClose={<FaHome />}
           url="/"
-          onClick={() => {
-            const element = document.getElementById("TopSection");
-            window.scrollTo({
-              top: element?.offsetTop,
-              behavior: "smooth",
-            });
-          }}
+          focus={focus[0]}
+          onClick={() => setScrollTo(0)}
         />
         <NavButton
           title="Projects"
-          className="mx-12"
+          className="mx-7"
           iconTypeOpen={<FaFolderOpen />}
           iconTypeClose={<FaFolder />}
-          onClick={() => {
-            const element = document.getElementById("ProjectSection");
-            window.scrollTo({
-              top: element?.offsetTop,
-              behavior: "smooth",
-            });
-          }}
+          url="/"
+          focus={focus[1]}
+          onClick={() => setScrollTo(1)}
         />
         <NavButton
           title="About me"
           iconTypeOpen={<FaBlogger />}
           iconTypeClose={<FaBlogger />}
-          onClick={() => {
-            const element = document.getElementById("AboutSection");
-            window.scrollTo({
-              top: element?.offsetTop,
-              behavior: "smooth",
-            });
-          }}
+          url="/"
+          focus={focus[2]}
+          onClick={() => setScrollTo(2)}
         />
       </div>
-      <div className="text-lg">
+      <div className="mr-2 text-lg">
         <Link href={"#"}>
           <span className="hover:underline">Projects</span>
         </Link>
