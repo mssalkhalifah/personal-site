98c885afb619966e6d5d4035d8141920668ea4c1
Mohammad Alkhalifah <60398196+mssalkhalifah@users.noreply.github.com>
2023-03-14 13:15:27 +0300

    feat: Added loading screen for /projects route

--- src/app/projects/loading.tsx
+++ src/app/projects/loading.tsx
@@ -1,35 +1,9 @@
 export default function Loading() {
   return (
-    <div className="flex w-[583.9px] animate-pulse flex-col justify-center">
-      <div className="h-10 w-full bg-zinc-200"></div>
-      <div className="mt-12 h-6 w-60 bg-zinc-200"></div>
-
-      <div className="mt-10 w-full space-y-2">
-        <div className="h-4 w-full bg-zinc-200"></div>
-        <div className="h-4 w-full bg-zinc-200"></div>
-        <div className="h-4 w-full bg-zinc-200"></div>
-        <div className="flex w-full space-x-2">
-          <div className="h-4 w-1/4 bg-zinc-200"></div>
-          <div className="h-4 w-9/12 bg-zinc-200"></div>
-        </div>
-        <div className="h-4 w-full bg-zinc-200"></div>
-        <div className="h-4 w-full bg-zinc-200"></div>
-      </div>
-
-      <div className="mt-10 w-full space-y-2">
-        <div className="h-4 w-full bg-zinc-200"></div>
-        <div className="flex w-full space-x-2">
-          <div className="h-4 w-1/2 bg-zinc-200"></div>
-          <div className="h-4 w-1/2 bg-zinc-200"></div>
-        </div>
-        <div className="h-4 w-full bg-zinc-200"></div>
-        <div className="flex w-full space-x-2">
-          <div className="h-4 w-9/12 bg-zinc-200"></div>
-          <div className="h-4 w-1/4 bg-zinc-200"></div>
-        </div>
-        <div className="h-4 w-full bg-zinc-200"></div>
-        <div className="h-4 w-full bg-zinc-200"></div>
-      </div>
+    <div className="mx-auto max-w-2xl animate-pulse justify-center space-y-3">
+      <div className="h-60 w-full bg-zinc-200"></div>
+      <div className="h-60 w-full bg-zinc-200"></div>
+      <div className="h-60 w-full bg-zinc-200"></div>
     </div>
   );
 }
