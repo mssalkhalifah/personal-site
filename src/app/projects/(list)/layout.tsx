638ca37e17c331fc5644abfde4099e1c2ab247cf
Mohammad Alkhalifah <60398196+mssalkhalifah@users.noreply.github.com>
2023-10-08 09:54:09 +0300

    chore: removed unused code

--- src/app/projects/(list)/layout.tsx
+++ src/app/projects/(list)/layout.tsx
@@ -1,31 +1,11 @@
-import { IPostCard } from "@/components/cards/post/PostCard";
-import { getProjectHeads } from "@/lib/projects";
-
 export default async function layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const projectHeads = await getProjectHeads();
-  const postList: IPostCard[] = [];
-  projectHeads.forEach((head) => {
-    postList.push({
-      title: head.title,
-      url: "projects/" + head.url,
-      imageUrl: head.imageThumb,
-      imageAlt: head.title,
-      description: head.description,
-      date: head.date,
-      tags: head.tags,
-    });
-  });
-
   return (
     <div className="container">
       <div className="prose prose-lg max-w-none">{children}</div>
     </div>
   );
 }
-// <div className="hidden lg:col-span-2 lg:block">
-//   <PostList postCards={postList} />
-// </div>
